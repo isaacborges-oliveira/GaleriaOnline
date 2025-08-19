575ac52161f0ae578bc7a0a8e5059f5e4895022a
Isaac Borges Oliveira <isaac.b.oliveira@aluno.senai.br>
2025-08-19 10:56:16 -0300

    :tadal: subindo o inicio no galeriaonline

---
+++ GaleriaOnline.Web.Api/GaleriaOnline.Web.Api/DbContextImagem/GaleriaOnlineDBContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using GaleriaOnline.Web.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GaleriaOnline.Web.Api.DbContextImagem;
+
+public partial class GaleriaOnlineDBContext : DbContext
+{
+    public GaleriaOnlineDBContext()
+    {
+    }
+
+    public GaleriaOnlineDBContext(DbContextOptions<GaleriaOnlineDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Imagem> Imagens { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=NOTE39-S28\\SQLEXPRESS;DataBase=GaleriaOnlineDB;user ID= sa; Pwd= Senai@134;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Imagem>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Imagens__3214EC07CA36900A");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ GaleriaOnline.Web.Api/GaleriaOnline.Web.Api/Models/Imagem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GaleriaOnline.Web.Api.Models;
+
+public partial class Imagem
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column("nome")]
+    [StringLength(255)]
+    public string Nome { get; set; } = null!;
+
+    [StringLength(500)]
+    public string Caminho { get; set; } = null!;
+}
